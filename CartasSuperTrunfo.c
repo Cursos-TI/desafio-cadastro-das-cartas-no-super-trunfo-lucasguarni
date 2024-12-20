e2c3cf3bb7ce296d08007987301190c9569af209
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2024-12-20 15:39:58 -0300

    feat: Adiciona cadastro de cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,40 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char name[100];
+    int population, touristAttractions;
+    float pib, area;
+
+    printf("Iniciando cadastro de cidade \n\n");
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    /*
+        A expressao utilizada no scanf recupera valor digitado ate acontecer uma quebra de linha (\n). 
+        Utilizando o %s a aplicacao quebra quando o nome da cidade tem mais de uma palavra. Como por exemplo Ribeirao Preto.
+    */ 
+    printf("Digite o nome da cidade: \n");
+    scanf(" %[^\n]", name); 
+
+    // Insere informacoes da populacao
+    printf("Digite a população de %s:\n", name);
+    scanf("%d", &population);
+
+    // Insere dados da area
+    printf("Digite a area de %s:\n", name);
+    scanf("%f", &area);
+
+    // Insere dados da PIB
+    printf("Digite o PIB de %s:\n", name);
+    scanf("%f", &pib);
+
+    // Insere dados da quantidade de pontos turisticos
+    printf("Digite a quantidade de pontos turísticos de %s:\n", name);
+    scanf("%d", &touristAttractions);
+
+    printf("Informacoes Gerais de %s: \n\n", name);
+    printf("População: %d\n", population);
+    printf("Area: %f\n", area);
+    printf("Pib: %f\n", pib);
+    printf("Numero de pontos turísticos: %d\n", touristAttractions);
 
     return 0;
 }
