a078f95524ccdf0d8732106946ae94a64f93574c
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2025-02-26 17:28:21 -0300

    feat: Finaliza nivel mestre
    Adiciona comparações de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,100 +24,108 @@ float calculateSuperPower(City city) {
     return city.population + city.touristAttractions + city.pib + city.pibPerCapita + city.area + city.populationDensity;
 }
 
-void setCity(City *city) {
+City setCity() {
+    City city;
     printf("Digite o nome da cidade: \n");
-    fgets(city->name, sizeof(city->name), stdin);
-    city->name[strcspn(city->name, "\n")] = '\0'; // Remove o caractere de nova linha
+    fgets(city.name, sizeof(city.name), stdin);
+
+    // Remove o caractere de nova linha
+    city.name[strcspn(city.name, "\n")] = '\0'; 
 
     // Insere informacoes da populacao
-    printf("Digite a população de %s:\n", city->name);
-    scanf("%lu", &city->population);
-    while (getchar() != '\n'); // Limpa o buffer de entrada
+    printf("Digite a população de %s:\n", city.name);
+    scanf("%lu", &city.population);
 
     // Insere dados da area
-    printf("Digite a area de %s:\n", city->name);
-    scanf("%f", &city->area);
-    while (getchar() != '\n'); // Limpa o buffer de entrada
+    printf("Digite a area de %s:\n", city.name);
+    scanf("%f", &city.area);
 
     // Insere dados da PIB
-    printf("Digite o PIB de %s:\n", city->name);
-    scanf("%f", &city->pib);
-    while (getchar() != '\n'); // Limpa o buffer de entrada
+    printf("Digite o PIB de %s:\n", city.name);
+    scanf("%f", &city.pib);
 
     // Insere dados da quantidade de pontos turisticos
-    printf("Digite a quantidade de pontos turísticos de %s:\n", city->name);
-    scanf("%d", &city->touristAttractions);
-    while (getchar() != '\n'); // Limpa o buffer de entrada
+    printf("Digite a quantidade de pontos turísticos de %s:\n", city.name);
+    scanf("%d", &city.touristAttractions);
+
+    // Limpa o buffer de entrada
+    while (getchar() != '\n');
 
     // Calculo de densidade populacional
-    city->populationDensity = calculatePopulationDensity(city->population, city->area);
+    city.populationDensity = calculatePopulationDensity(city.population, city.area);
 
     // Calculo PIB per capita
-    city->pibPerCapita = calculatePibPerCapita(city->population, city->pib);
+    city.pibPerCapita = calculatePibPerCapita(city.population, city.pib);
 
     // Calculo do super poder
-    city->superPower = calculateSuperPower(*city);
+    city.superPower = calculateSuperPower(city);
 
     // Exibe todas as informações
-    printf("Informacoes Gerais de %s: \n\n", city->name);
-    printf("População: %lu\n", city->population);
-    printf("Area: %f\n", city->area);
-    printf("Pib: %f\n", city->pib);
-    printf("Numero de pontos turísticos: %d\n", city->touristAttractions);
-    printf("Densidade Populacional: %f\n", city->populationDensity);
-    printf("PIB per Capita: %f\n", city->pibPerCapita);
-    printf("Super Poder: %f\n", city->superPower);
+    printf("Informacoes Gerais de %s: \n\n", city.name);
+    printf("População: %lu\n", city.population);
+    printf("Area: %f\n", city.area);
+    printf("Pib: %f\n", city.pib);
+    printf("Numero de pontos turísticos: %d\n", city.touristAttractions);
+    printf("Densidade Populacional: %f\n", city.populationDensity);
+    printf("PIB per Capita: %f\n", city.pibPerCapita);
+    printf("Super Poder: %f\n", city.superPower);
+
+    return city;
 }
 
 int main() {
     City city1, city2;
 
-    printf("Cadastro da primeira cidade:\n");
-    setCity(&city1);
+    printf("Cartas SuperTrufo de cidades:\n\n");
 
-    printf("\nCadastro da segunda cidade:\n");
-    setCity(&city2);
+    printf("Cadastro da primeira carta:\n");
+    city1 = setCity();
+
+    printf("\nCadastro da segunda carta:\n");
+    city2 = setCity();
+
+    printf("\nResultado da partida:\n");
 
     if (city1.population > city2.population) {
-        printf("A cidade %s tem a maior população\n", city1.name);
+        printf("A carta %s tem a maior população\n", city1.name);
     } else {
-        printf("A cidade %s tem a maior população\n", city2.name);
+        printf("A carta %s tem a maior população\n", city2.name);
     }
 
     if (city1.area > city2.area) {
-        printf("A cidade %s tem a maior área\n", city1.name);
+        printf("A carta %s tem a maior área\n", city1.name);
     } else {
-        printf("A cidade %s tem a maior área\n", city2.name);
+        printf("A carta %s tem a maior área\n", city2.name);
     }
 
     if(city1.pib > city2.pib) {
-        printf("A cidade %s tem o maior PIB\n", city1.name);
+        printf("A carta %s tem o maior PIB\n", city1.name);
     } else {
-        printf("A cidade %s tem o maior PIB\n", city2.name);
+        printf("A carta %s tem o maior PIB\n", city2.name);
     }
 
     if (city1.touristAttractions > city2.touristAttractions) {
-        printf("A cidade %s tem mais pontos turísticos\n", city1.name);
+        printf("A carta %s tem mais pontos turísticos\n", city1.name);
     } else {
-        printf("A cidade %s tem mais pontos turísticos\n", city2.name);
+        printf("A carta %s tem mais pontos turísticos\n", city2.name);
     }
 
     if(city1.populationDensity < city2.populationDensity) {
-        printf("A cidade %s tem a maior densidade populacional\n", city1.name);
+        printf("A carta %s tem a maior densidade populacional\n", city1.name);
     } else {
-        printf("A cidade %s tem a maior densidade populacional\n", city2.name);
+        printf("A carta %s tem a maior densidade populacional\n", city2.name);
     }
 
     if(city1.pibPerCapita > city2.pibPerCapita) {
-        printf("A cidade %s tem o maior PIB per capita\n", city1.name);
+        printf("A carta %s tem o maior PIB per capita\n", city1.name);
     } else {
-        printf("A cidade %s tem o maior PIB per capita\n", city2.name);
+        printf("A carta %s tem o maior PIB per capita\n", city2.name);
     }
 
     if(city1.superPower > city2.superPower) {
-        printf("A cidade %s tem o maior super poder\n", city1.name);
+        printf("A carta %s tem o maior super poder\n", city1.name);
     } else {
-        printf("A cidade %s tem o maior super poder\n", city2.name);
+        printf("A carta %s tem o maior super poder\n", city2.name);
     }
     
     return 0;
